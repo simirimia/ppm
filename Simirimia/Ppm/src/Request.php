212d59234363055a749e5ddcac41598363163052
simirimia <simirimia@triosolutions.at>
2014-10-14 23:44:17 +0200

    adding request class

---
+++ Simirimia/Ppm/src/Request.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * This file is part of PPM by simirimia
+ * 
+ * Date: 14.10.14
+ * Time: 23:02
+ */
+
+namespace Simirimia\Ppm;
+
+
+class Request
+{
+    /**
+     * @var string
+     */
+    private $url;
+    /***
+     * @var int
+     */
+    private $page;
+    /**
+     * @var int
+     */
+    private $pageSize;
+
+    public static function createFromSuperGlobals()
+    {
+        if (isset($_SERVER['REQUEST_URI'])) {
+            $url = $_SERVER['REQUEST_URI'];
+        } elseif ($_SERVER['argv'][2]) {
+            $url = $_SERVER['argv'][2];
+        } else {
+            //$url = "/rest/pictures/thumbnails/create";
+            //$url = "/rest/pictures/thumbnails/small";
+            //$url = '/rest/pictures/scan';
+            //$url = '/rest/picture/extract-exif';
+            $url = '';
+        }
+        $url = explode( '?', $url );
+        $url = array_shift( $url );
+
+        return new Request( $url, $_GET );
+    }
+
+    public function __construct( $url, array $queryParams )
+    {
+        $this->url = (string)$url;
+
+        $this->page = isset($queryParams['page']) ? $queryParams['page'] : 1;
+        $this->pageSize = isset($queryParams['pageSize']) ? $queryParams['pageSize'] : 20;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPage()
+    {
+        return $this->page;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPageSize()
+    {
+        return $this->pageSize;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+
+} 
