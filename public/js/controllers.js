fb8dccc707dd7ce343a9622b2f6d0ebd01e14409
simirimia <simirimia@triosolutions.at>
2014-10-15 08:12:40 +0200

    working pagination

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,25 +2,29 @@ var ppmApp = angular.module('ppmApp', ['ui.bootstrap']);
 
 ppmApp.controller('PpmCtrl', function ($scope, $http) {
 
-    $http.get('/rest/pictures/thumbnails/small').success(function(data) {
-        $scope.thumbnails = data;
-    });
+    $scope.thumbnailsCurrentPage = 1;
+    $scope.thumbnailsPageSize = 20;
 
+
+    $scope.thumbnailsPageChanged = function() {
+        console.log( 'New thumbnail page: ' + $scope.thumbnailsCurrentPage );
+        $scope.loadThumbnails( $scope );
+    }
+
+    $scope.setPage = function( pageNumber ) {
+        console.log( 'setting page to: ' + pageNumber );
+        $scope.currentPage = pageNumber;
+    }
+
+    $scope.loadThumbnails = function( $scope ) {
+        $http.get('/rest/pictures/thumbnails/small?page=' + $scope.thumbnailsCurrentPage + '&pageSize=' + $scope.thumbnailsPageSize).success(function(data) {
+            $scope.thumbnails = data;
+        });
+    }
+
+
+    // init calls
+
+    $scope.loadThumbnails( $scope );
 });
 
-ppmApp.controller('PaginationDemoCtrl', function ($scope) {
-    $scope.totalItems = 64;
-    $scope.currentPage = 4;
-
-    $scope.setPage = function (pageNo) {
-        $scope.currentPage = pageNo;
-    };
-
-    $scope.pageChanged = function() {
-        console.log('Page changed to: ' + $scope.currentPage);
-    };
-
-    $scope.maxSize = 5;
-    $scope.bigTotalItems = 175;
-    $scope.bigCurrentPage = 1;
-});
