3f51ce9ed9e6a042304a5416929895e2ef2ece32
simirimia <simirimia@triosolutions.at>
2015-03-30 14:26:05 +0200

    remove modal (wasn't working anyway) and ui.bootstrap --> start upgrade to angular 1.3

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,4 @@
 var ppmApp = angular.module('ppmApp', [
-    'ui.bootstrap',
     'ngRoute',
     'ngDraggable',
     'infinite-scroll',
@@ -91,44 +90,3 @@ ppmApp.factory( 'UserService', function () {
         }
     };
 } );
-
-/*
-angular.module('ppmApp').run(['$rootScope',function($rootScope){
-    $rootScope.$on('$stateChangeStart',function(event, toState, toParams, fromState, fromParams){
-        console.log('$stateChangeStart to '+toState.to+'- fired when the transition begins. toState,toParams : \n',toState, toParams);
-    });
-    $rootScope.$on('$stateChangeError',function(event, toState, toParams, fromState, fromParams, error){
-        console.log('$stateChangeError - fired when an error occurs during transition.');
-        console.log(arguments);
-    });
-    $rootScope.$on('$stateChangeSuccess',function(event, toState, toParams, fromState, fromParams){
-        console.log('$stateChangeSuccess to '+toState.name+'- fired once the state transition is complete.');
-    });
-    $rootScope.$on('$viewContentLoaded',function(event){
-        console.log('$viewContentLoaded - fired after dom rendered',event);
-    });
-    $rootScope.$on('$stateNotFound',function(event, unfoundState, fromState, fromParams){
-        console.log('$stateNotFound '+unfoundState.to+'  - fired when a state cannot be found by its name.');
-        console.log(unfoundState, fromState, fromParams);
-    });
-    $rootScope.$on('$routeChangeError', function(current, previous, rejection) {
-        console.log("routeChangeError", currrent, previous, rejection);
-    });
-
-    $rootScope.$on('$routeChangeStart', function(next, current) {
-        console.log("routeChangeStart");
-        console.dir(next);
-        console.dir(current);
-    });
-
-    $rootScope.$on('$routeChangeSuccess', function(current, previous) {
-        console.log("routeChangeSuccess");
-        console.dir(current);
-        console.dir(previous);
-    });
-
-    $rootScope.$on('$routeUpdate', function(rootScope) {
-        console.log("routeUpdate", rootScope);
-    });
-}]);
-    */
--- public/js/controllers-thumbnail-list.js
+++ public/js/controllers-thumbnail-list.js
@@ -4,7 +4,7 @@ var ppmThumbnailListControllers = angular.module('ppmThumbnailListControllers', 
 /**
  * Thumbnail view which shows thumbnails for all pictures
  */
-ppmThumbnailListControllers.controller('ThumbnailListCtrl', function ($scope, $http, $modal, $location, $window) {
+ppmThumbnailListControllers.controller('ThumbnailListCtrl', function ($scope, $http, $location, $window) {
 
     $scope.additionalPicturesAvailable = true;
 
@@ -14,29 +14,29 @@ ppmThumbnailListControllers.controller('ThumbnailListCtrl', function ($scope, $h
         });
     };
 
-    $scope.appendMoreThumbnails = function() {
+    $scope.appendMoreThumbnails = function () {
         $http.get('/rest/pictures/thumbnails/small?page=' + $scope.thumbnailsCurrentPage + '&pageSize=' + $scope.thumbnailsPageSize).success(function (data) {
 
-            if ( jQuery.isEmptyObject( data ) ) {
+            if (jQuery.isEmptyObject(data)) {
                 $scope.additionalPicturesAvailable = false;
             } else {
-                jQuery.extend( $scope.thumbnails, data );
+                jQuery.extend($scope.thumbnails, data);
                 $scope.additionalPicturesAvailable = true;
             }
 
         });
-    }
+    };
 
 
     // init calls
-    ppmThumbnailListControllers.ThumbnailListHelper_init($scope, $modal, $location, $http, $window);
+    ppmThumbnailListControllers.ThumbnailListHelper_init($scope, $location, $http, $window);
     $scope.loadThumbnails($scope);
 });
 
 /**
  * Thumbnail view which shows thumbnails for all pictures with a given tag
  */
-ppmThumbnailListControllers.controller('ThumbnailsListByTagCtrl', function ($scope, $http, $modal, $routeParams, $location, $window) {
+ppmThumbnailListControllers.controller('ThumbnailsListByTagCtrl', function ($scope, $http, $routeParams, $location, $window) {
 
     $scope.additionalPicturesAvailable = true;
 
@@ -49,10 +49,10 @@ ppmThumbnailListControllers.controller('ThumbnailsListByTagCtrl', function ($sco
     $scope.appendMoreThumbnails = function () {
         $http.get('/rest/tags/' + $routeParams.tag + '/thumbnails/small?page=' + $scope.thumbnailsCurrentPage + '&pageSize=' + $scope.thumbnailsPageSize).success(function (data) {
 
-            if ( jQuery.isEmptyObject( data ) ) {
+            if (jQuery.isEmptyObject(data)) {
                 $scope.additionalPicturesAvailable = false;
             } else {
-                jQuery.extend( $scope.thumbnails, data );
+                jQuery.extend($scope.thumbnails, data);
                 $scope.additionalPicturesAvailable = true;
             }
         });
@@ -61,7 +61,7 @@ ppmThumbnailListControllers.controller('ThumbnailsListByTagCtrl', function ($sco
     $scope.selectedTag = $routeParams.tag;
 
     // init calls
-    ppmThumbnailListControllers.ThumbnailListHelper_init($scope, $modal, $location, $http, $window);
+    ppmThumbnailListControllers.ThumbnailListHelper_init($scope, $location, $http, $window);
 
 
     $scope.loadThumbnails($scope);
@@ -72,11 +72,10 @@ ppmThumbnailListControllers.controller('ThumbnailsListByTagCtrl', function ($sco
 /**
  * Base stuff done for all controller which produce a thumbnail list as output
  */
-ppmThumbnailListControllers.ThumbnailListHelper_init = function ($scope, $modal, $location, $http, $window) {
+ppmThumbnailListControllers.ThumbnailListHelper_init = function ($scope, $location, $http, $window) {
 
     $scope.thumbnailsCurrentPage = 1;
     $scope.thumbnailsPageSize = 20;
-    $scope.mainPicture = { id: 0, href: '' };
     $scope.showTags = true;
     $scope.showMarker = true;
 
@@ -111,137 +110,106 @@ ppmThumbnailListControllers.ThumbnailListHelper_init = function ($scope, $modal,
         $window.open('http://localhost/#/pictures/' + pictureId);
     };
 
-    $scope.addTag = function ( pictureId, tag ) {
-        console.log( 'Adding tag ' + tag + ' to picture with ID ' + pictureId );
-        $http.post( '/rest/pictures/' + pictureId + '/tags', tag).success( function(data) {
-            console.log( 'Tag update returned: ' + data );
+    $scope.addTag = function (pictureId, tag) {
+        console.log('Adding tag ' + tag + ' to picture with ID ' + pictureId);
+        $http.post('/rest/pictures/' + pictureId + '/tags', tag).success(function (data) {
+            console.log('Tag update returned: ' + data);
             $scope.thumbnails[pictureId].tags = data.tags;
-        } );
+        });
     };
 
-    $scope.removeTag = function ( pictureId, tag ) {
-        console.log( 'Removing tag ' + tag + ' to picture with ID ' + pictureId );
-        $http.delete( '/rest/pictures/' + pictureId + '/tags/' + tag).success( function(data) {
-            console.log( 'Tag removal returned: ', data );
+    $scope.removeTag = function (pictureId, tag) {
+        console.log('Removing tag ' + tag + ' to picture with ID ' + pictureId);
+        $http.delete('/rest/pictures/' + pictureId + '/tags/' + tag).success(function (data) {
+            console.log('Tag removal returned: ', data);
             $scope.thumbnails[pictureId].tags = data.tags;
-        } );
+        });
     };
 
-    $scope.hasTag = function( tag, tags ) {
+    $scope.hasTag = function (tag, tags) {
         var returnValue = false;
-        tags.forEach( function( e ) {
-            if ( e == tag ) {
+        tags.forEach(function (e) {
+            if (e == tag) {
                 returnValue = true;
             }
-        } );
+        });
         return returnValue;
     };
 
-    $scope.deleteThumbnails = function( pictureId ) {
-        console.log( 'deleting thumbnails for picture ' + pictureId );
-        $http.delete( '/rest/pictures/' + pictureId + '/thumbnails').success( function(data) {
-            console.log( 'thumbnail success deletion returned: ', data );
+    $scope.deleteThumbnails = function (pictureId) {
+        console.log('deleting thumbnails for picture ' + pictureId);
+        $http.delete('/rest/pictures/' + pictureId + '/thumbnails').success(function (data) {
+            console.log('thumbnail success deletion returned: ', data);
             $scope.alertTypeClass = 'alert-success';
             $scope.showAlert = true;
             $scope.alertMessage = 'Thumbnails are deleted - reload page necessary :-(';
             $scope.thumbnails[pictureId].href = null;
-        }).error( function(data) {
-            console.log( 'thumbnail error deletion returned: ', data );
+        }).error(function (data) {
+            console.log('thumbnail error deletion returned: ', data);
             $scope.alertTypeClass = 'alert-danger';
             $scope.showAlert = true;
             $scope.alertMessage = data.message;
-        } )
+        })
     };
 
-    $scope.createThumbnails = function( pictureId ) {
-        console.log( 'creating thumbnails for picture ' + pictureId );
-        $http.post( '/rest/pictures/' + pictureId + '/thumbnails', null).success( function(data) {
-            console.log( 'thumbnail success creation returned: ', data );
+    $scope.createThumbnails = function (pictureId) {
+        console.log('creating thumbnails for picture ' + pictureId);
+        $http.post('/rest/pictures/' + pictureId + '/thumbnails', null).success(function (data) {
+            console.log('thumbnail success creation returned: ', data);
             $scope.alertTypeClass = 'alert-success';
             $scope.showAlert = true;
             $scope.alertMessage = 'Thumbnails are created - reload page necessary :-(';
             $scope.thumbnails[pictureId].href = null;
-        }).error( function(data) {
-            console.log( 'thumbnail error creation returned: ', data );
+        }).error(function (data) {
+            console.log('thumbnail error creation returned: ', data);
             $scope.alertTypeClass = 'alert-danger';
             $scope.showAlert = true;
             $scope.alertMessage = data.message;
-        } )
+        })
     };
 
-    $scope.rotateThumbnails = function( pictureId, rotationType ) {
-        console.log( 'rotating thumbs for picture ' + pictureId + ' in direction: ' + rotationType );
-        $http.post( '/rest/pictures/' + pictureId + '/thumbnails/rotate', rotationType).success( function(data) {
+    $scope.rotateThumbnails = function (pictureId, rotationType) {
+        console.log('rotating thumbs for picture ' + pictureId + ' in direction: ' + rotationType);
+        $http.post('/rest/pictures/' + pictureId + '/thumbnails/rotate', rotationType).success(function (data) {
             console.log('thumb rotate success', data);
             $scope.alertTypeClass = 'alert-success';
             $scope.showAlert = true;
             $scope.alertMessage = 'Thumbnails are rotated - reload page necessary :-(';
-        }).error( function(data) {
-            console.log( 'thumb rotate error', data );
+        }).error(function (data) {
+            console.log('thumb rotate error', data);
             $scope.alertTypeClass = 'alert-danger';
             $scope.showAlert = true;
             $scope.alertMessage = data.message;
-        } )
+        })
     };
 
-    $scope.rotatateThumbnailsClockwise = function( pictureId ) {
-        $scope.rotateThumbnails( pictureId, 'clockwise' );
+    $scope.rotatateThumbnailsClockwise = function (pictureId) {
+        $scope.rotateThumbnails(pictureId, 'clockwise');
     };
-    $scope.rotatateThumbnailsCounterClockwise = function( pictureId ) {
-        $scope.rotateThumbnails( pictureId, 'counterclockwise' );
+    $scope.rotatateThumbnailsCounterClockwise = function (pictureId) {
+        $scope.rotateThumbnails(pictureId, 'counterclockwise');
     };
 
-    $scope.onDragComplete=function(data,evt){
+    $scope.onDragComplete = function (data, evt) {
         console.log("drag success, data:", data);
     };
 
-    $scope.onDropComplete=function( pictureId ,evt ){
+    $scope.onDropComplete = function (pictureId, evt) {
         console.log("drop success, source data", pictureId);
-        if ( $scope.mainPicture.id == 0 ) {
+        if ($scope.mainPicture.id == 0) {
             console.log('setting new main picture');
             $scope.mainPicture = $scope.thumbnails[pictureId];
         } else {
-            console.log( 'Marking as Alternative' );
-            $http.post( 'rest/pictures/' + $scope.mainPicture.id + '/alternatives', pictureId).success(
-                function(data) {
-                    console.log( 'Adding alternative returnded: ', data );
+            console.log('Marking as Alternative');
+            $http.post('rest/pictures/' + $scope.mainPicture.id + '/alternatives', pictureId).success(
+                function (data) {
+                    console.log('Adding alternative returnded: ', data);
                 }
             );
         }
     };
 
-    $scope.resetMainPicture = function() {
-        console.log('reset main picture. Was:', $scope.mainPicture);
-        $scope.mainPicture = { id: 0, href: '' };
-    };
-
-    $scope.previewSize = 800;
-    $scope.showThumbnailModal = function (pictureId) {
-        var modalInstance = $modal.open({
-            templateUrl: 'thumbnailPreview.html',
-            controller: 'ThumbnailPreviewInstanceCtrl',
-            size: 'lg',
-            resolve: {
-                pictureId: function () {
-                    return pictureId;
-                },
-                size: function () {
-                    return $scope.previewSize;
-                }
-            },
-            backdrop: true
-        });
-
-        modalInstance.result.then(function (ret) {
-            $scope.previewSize = ret[1];
-            $scope.showThumbnailModal(ret[0]);
-        }, function () {
-            //$scope.previewSize = 800;
-        });
-
-    };
-
-    $scope.myPagingFunction = function() {
+    $scope.myPagingFunction = function () {
         $scope.thumbnailsCurrentPage++;
         $scope.appendMoreThumbnails();
     };
@@ -249,25 +217,4 @@ ppmThumbnailListControllers.ThumbnailListHelper_init = function ($scope, $modal,
 };
 
 
-// controller for overlay with bigger preview within the thumbnail list view
-ppmThumbnailListControllers.controller('ThumbnailPreviewInstanceCtrl', function ($scope, $modalInstance, $window, pictureId, size) {
-    $scope.pictureId = pictureId;
-    $scope.size = size;
 
-    $scope.showLarge = function () {
-        $modalInstance.close([ pictureId, 1400 ]);
-    };
-
-    $scope.showSmall = function () {
-        $modalInstance.close([ pictureId, 800 ]);
-    };
-
-    $scope.close = function () {
-        $modalInstance.dismiss('close');
-    };
-
-    $scope.showOriginal = function () {
-        $window.open('/rest/pictures/' + pictureId + '/original');
-    }
-
-});
